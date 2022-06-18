18859668f216aa41e4cb06de591dfb9668a2c5d6
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-19 01:28:07 +0800

    Delete .storybook directory

--- .storybook/preview.js
+++
@@ -1,8 +0,0 @@
-import "../src/taskpane/taskpane.css";
-import { initializeIcons } from "office-ui-fabric-react/lib/Icons";
-
-initializeIcons();
-
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" }
-};
